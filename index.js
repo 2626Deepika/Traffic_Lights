54e2c18622352a9ee0f0443c995166b49f1db207
Deepika <159604970+2626Deepika@users.noreply.github.com>
2025-07-21 11:34:34 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
-let stopp=document.getElementById("stop");
+let stopp=document.getElementById("stopp");
 let ready=document.getElementById("ready");
 let go=document.getElementById("go");
 let l1=document.getElementById("l1");
 let l2=document.getElementById("l2")
 let l3=document.getElementById("l3");
-let p=document.getElementById("pa")
+let p=document.getElementById("pa");
+let im=document.getElementById("ima");
 stopp.onclick=()=>{
     stopp.style.backgroundColor='red';
     ready.style.backgroundColor='navy'
@@ -12,7 +13,7 @@ stopp.onclick=()=>{
     l1.style.backgroundColor='red';
      l2.style.backgroundColor='navy';
       l3.style.backgroundColor='navy';
-      p.textContent='Please Stop And Relax'
+    im.src="https://www.creativehatti.com/wp-content/uploads/edd/2021/12/Traffic-policeman-is-holding-stop-signboard-and-doing-a-stop-hand-sign-9-large.jpg";
 
 }
 ready.onclick=()=>{
@@ -22,7 +23,7 @@ ready.onclick=()=>{
     l1.style.backgroundColor='navy';
      l2.style.backgroundColor='orange';
       l3.style.backgroundColor='navy';
-      p.textContent="Get Ready To Go";
+      im.src="https://uttarakhandtraffic.com/UserFiles/images/traffic-police/seat-belt-img1.jpg";
 }
 go.onclick=()=>{
      go.style.backgroundColor='green';
@@ -31,6 +32,6 @@ go.onclick=()=>{
     l1.style.backgroundColor='navy';
      l2.style.backgroundColor='navy';
       l3.style.backgroundColor='green';
-      p.textContent="All Set to Gooooo"
+    im.src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQOJjYmHRksS9wJvEh67uAufa6L5Zb59_CPNQ&s";
     
 }
